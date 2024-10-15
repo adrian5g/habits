42b2961131f6b00aa536361fee029aa495fe39fe
adrian5g <jogadorsquarebr@gmail.com>
2024-10-15 10:52:16 -0300

    web setup final

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,8 @@ import Fastify from 'fastify';
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client';
 
-const app = Fastify();
-const prisma = new PrismaClient()
+export const app = Fastify();
+export const prisma = new PrismaClient()
 
 app.register(cors)
 
