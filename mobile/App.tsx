e7d6af9e39c0529822b3c41ef67a843549de7eb5
adrian5g <jogadorsquarebr@gmail.com>
2024-10-16 16:54:56 -0300

    mobile setup

---
+++ mobile/App.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, StatusBar, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  // se as fonts não estiverem carregadas o app não inicia
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
+      <Text style={styles.text}>
+        Apenas um teste
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Inter_600SemiBold'
+  },
+});
